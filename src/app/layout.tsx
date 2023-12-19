d3e79be52fb936ce2fa1291fcdcd8b7569873bb4
Thomas Thachil <github@thomasthachil.com>
2023-12-18 22:42:08 -0500

    add prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning>
         <head />
         <body>
-        <ThemeProvider
+          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
